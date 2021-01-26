ffa6a9de62f74545fac160ee91d2e810c28f1dd2
CESCFAB <cescfab@Naver.com>
2021-01-26 17:29:17 +0900

    01-26 종한 : 회원가입 첫번째 페이지 디비연동까지

---
+++ AndroidPJ_MAIN/app/src/main/java/com/android/androidpj_main/UserSign/SignupActivity_2.java
@@ -0,0 +1,16 @@
+package com.android.androidpj_main.UserSign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.android.androidpj_main.R;
+
+public class SignupActivity_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_2);
+    }
+}
